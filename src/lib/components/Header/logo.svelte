ab5c376f33f5ffd28cf0f7f51038b1d6b362616f
Enes Buzluk <139135923+cysctl@users.noreply.github.com>
2026-03-17 12:28:49 +0300

    feat: complete Logo component

--- src/lib/components/Header/logo.svelte
+++ src/lib/components/Header/logo.svelte
@@ -1,3 +1,10 @@
-<div>
-	<span>Logo</span>
+<script lang="ts">
+	import { Activity } from '@lucide/svelte';
+</script>
+
+<div class="flex items-center gap-2">
+	<span>
+		<Activity size={20} />
+	</span>
+	<span class="font-semibold tracking-tighter uppercase">daq command center</span>
 </div>

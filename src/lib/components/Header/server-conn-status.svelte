25429d4815948f1da41f6c77f52ca61adbbcc725
Enes Buzluk <139135923+cysctl@users.noreply.github.com>
2026-03-17 12:55:54 +0300

    feat: complete responsive ServerConnStatus component

--- src/lib/components/Header/server-conn-status.svelte
+++ src/lib/components/Header/server-conn-status.svelte
@@ -1,3 +1,4 @@
-<div>
-	<span>Connected</span>
+<div class="inline-flex items-center gap-2">
+	<div class="size-2 animate-pulse rounded-full bg-state-run"></div>
+	<span class="hidden lg:block">Connected</span>
 </div>

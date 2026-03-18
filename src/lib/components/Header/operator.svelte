34c9b89daec082ed6d83538dd7749ed80f33fc8f
Enes Buzluk <139135923+cysctl@users.noreply.github.com>
2026-03-18 21:50:51 +0300

    feat: replace Operator with Settings component in Header

--- src/lib/components/Header/operator.svelte
+++
@@ -1,4 +0,0 @@
-<div>
-	<span class="hidden lg:inline">Operator:</span>
-	<span>John Doe</span>
-</div>
---
+++ src/lib/components/Header/settings.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Settings } from '@lucide/svelte';
+</script>
+
+<button
+	class="h-fit cursor-pointer rounded-lg p-1 text-muted-foreground transition-colors hover:bg-border active:scale-95"
+>
+	<Settings size={17} />
+</button>

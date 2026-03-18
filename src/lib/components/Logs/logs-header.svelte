aacd0a2834e795c3179a6252c7e47ae6a259e4dd
Enes Buzluk <139135923+cysctl@users.noreply.github.com>
2026-03-18 12:57:34 +0300

    refactor: make LogsHeader component responsive

--- src/lib/components/Logs/logs-header.svelte
+++ src/lib/components/Logs/logs-header.svelte
@@ -7,15 +7,17 @@
 	import { Terminal } from '@lucide/svelte';
 </script>
 
-<div class="flex items-center justify-between border-b border-border pb-4">
-	<div class="flex items-center gap-2">
+<div
+	class="flex flex-col gap-4 border-b border-border pb-4 md:flex-row md:items-start md:justify-between xl:items-center"
+>
+	<div class="flex items-center gap-2 md:mt-2 xl:mt-0">
 		<Terminal size={20} class="text-muted-foreground" />
 		<span class="font-medium tracking-wider text-muted-foreground uppercase">system logs</span>
 	</div>
-	<div class="flex flex-wrap items-stretch gap-3 relative">
+	<div class="relative flex flex-wrap items-center gap-3 md:justify-end">
 		<Search />
 		<FrontEndFilters />
-		<div class="h-6 w-px self-center bg-border mx-1"></div>
+		<div class="hidden h-6 w-px self-center bg-border sm:block"></div>
 		<SelectSender />
 		<NetworkLogLevel />
 	</div>

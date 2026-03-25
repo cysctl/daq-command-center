42312b35bcaab6d93f90ac5ae6521cbc60239f10
Enes Buzluk <139135923+cysctl@users.noreply.github.com>
2026-03-25 10:05:09 +0300

    feat: add non-functional theme toggle button

--- src/lib/components/Header/index.svelte
+++ src/lib/components/Header/index.svelte
@@ -3,12 +3,15 @@
 	import Settings from './settings.svelte';
 	import ServerConnStatus from './server-conn-status.svelte';
 	import Divider from '../ui/divider.svelte';
+	import ToggleTheme from './toggle-theme.svelte';
 </script>
 
 <header class="flex w-full justify-between border-b border-border p-5">
 	<Logo />
 	<div class="flex items-center gap-4">
 		<ServerConnStatus />
+		<Divider />
+		<ToggleTheme />
 		<Divider />
 		<Settings />
 	</div>
---
+++ src/lib/components/Header/toggle-theme.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Sun, Moon } from '@lucide/svelte';
+
+	let theme: 'light' | 'dark' = $state('dark');
+</script>
+
+<button
+	class="h-fit cursor-pointer rounded-lg p-1 text-muted-foreground transition-colors hover:bg-border active:scale-95"
+	onclick={() => (theme = theme == 'light' ? 'dark' : 'light')}
+>
+	{#if theme == 'dark'}
+		<Moon size={17} />
+	{:else}
+		<Sun size={17} />
+	{/if}
+</button>

d83eb9d6d38d70e459c63627ed75e541d9703c83
Enes Buzluk <139135923+cysctl@users.noreply.github.com>
2026-03-26 14:00:47 +0300

    refactor: some minor changes

--- src/lib/components/Satellites/quick-action-buttons.svelte
+++ src/lib/components/Satellites/quick-action-buttons.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <div class="hidden flex-wrap items-center gap-2 md:flex">
+	<span class="text-sm text-muted-foreground select-none">All:</span>
 	{#each buttons as label}
 		<button
 			class="cursor-pointer rounded-lg px-2 py-1 text-sm text-muted-foreground transition-colors select-none hover:bg-border active:scale-95"
